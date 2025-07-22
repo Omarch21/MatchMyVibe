ede957eb3996a6b4c7d6de67fd47d25dc65238bb
omarch21 <omarch641@gmail.com>
2025-07-21 21:50:06 -0500

    initial  2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AccountComponent } from './components/account/account.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        title: 'Home',
+        component: HomeComponent
+    },
+    {
+        path: 'account',
+        title: 'Account Settings',
+        component: AccountComponent
+    }
+];
